e0737f3b9e20a2cceb8d6db4de84751c2e86fe4e
Artour Fire <shurnd32281@gmail.com>
2026-04-28 15:08:04 +0300

    fix: fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = Number(process.env.PORT ?? 4000);
+  const allowedCors = [
+    'https://moonf.nomorepartiessite.ru',
+    'http://moonf.nomorepartiessite.ru',
+    'https://api.moonf.nomorepartiessite.ru',
+    'http://api.moonf.nomorepartiessite.ru',
+    'http://localhost:8081',
+    'http://localhost:3000',
+  ];
+
+  app.enableCors({
+    origin: allowedCors,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
